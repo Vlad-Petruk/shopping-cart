a517439ab42d350b151742a26c06a6fce0d0480c
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-07 18:15:13 +0300

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Home from "./components/Home/Home.jsx";
+import Shop from "./components/Shop/Shop.jsx";
+import Cart from "./components/Cart/Cart.jsx";
+import Product from "./components/Product/Product.jsx";
+import Error from "./components/Error/Error.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <Error />
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+        errorElement: <Error />,
+        children: [
+          {
+            path: "product/:id",
+            element: <Product />
+          }
+        ]
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+        errorElement: <Error />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
