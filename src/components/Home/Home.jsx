212e1090e83d6d4ab0812b016a1f754cb2eef4c4
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-07 18:17:37 +0300

    add and style photo section of main screen

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
+import styles from './Home.module.css'
 
 function Home() {
 
   return (
-    <>
-      
-    </>
+    <div className={styles.mainContainer}>
+      <div className={styles.info}>Info</div>
+      <div className={styles.photoBox}>
+        <img className={styles.img} src="/main-img.png" alt="" />
+      </div>
+    </div>
   );
 }
 
