17efab94c834d19d5b91235241417ed76b342216
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-07 14:59:00 +0300

    create navigation panel

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
+
+function NavBar() {
+
+    return (
+      <>
+        <div className={styles.navBar}>
+          <div className={styles.logo}>Logo</div>
+          <div className={styles.navElements}>
+            <Link to="/">
+              <div className={styles.home}>Home</div>
+            </Link>
+            <Link to="shop">
+              <div className={styles.shop}>Shop</div>
+            </Link>
+            <Link to="cart">
+            <div className={styles.cart}>Cart</div>
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default NavBar;
