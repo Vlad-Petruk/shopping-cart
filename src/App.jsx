0ef702662a4dd91161ba424a311f88c3728ffe1c
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-08 17:28:09 +0300

    write basic fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-// import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading]= useState(true)
+  function useFetch(parameters) {
+    useEffect(()=>{
+      fetch(`https://fakestoreapi.com/products${parameters}`,{ mode: 'cors' })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(data=>{
+          setData(data)
+          setLoading(false)
+          console.log(data)
+        })
+        .catch((error) => {
+          setError(error);
+          setLoading(false);
+        });
+    }, [parameters])
+  }
+
+  useFetch('')
+
   return (
     <>
       <NavBar />
