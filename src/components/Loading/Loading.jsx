1fe0947651d8899342fceeff1b83b3f175e115ec
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-12 17:34:23 +0300

    add Loading component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,11 @@
-
+import styles from "./Loading.module.css"
 
 function Loading () {
 
     return (
-        <></>
+        <div className={styles.loading}>
+            <img src="/Loading.svg" alt="" />
+        </div>
     )
 }
 
--- src/components/Shop/ProductBox.jsx
+++ src/components/Shop/ProductBox.jsx
@@ -1,26 +1,30 @@
-import styles from "./Shop.module.css"
-import { useOutletContext } from "react-router-dom";
+import styles from "./Shop.module.css";
 import Product from "../Product/Product";
+import Loading from "../Loading/Loading";
 
-function ProductBox() {
-  const { data, error, loading, useFetch } = useOutletContext();
-  useFetch('')
-
+function ProductBox({ data, error, loading }) {
   if (loading) {
-    return <p>Loading...</p>;
+    return <div className={styles.loading}><Loading /></div> ;
   }
 
   if (error) {
     return <p>Error: {error.message}</p>;
   }
-    return (
-      <div className={styles.productBox}>
-        {console.log(data)}
-        {data.map((product)=>(
-          <Product key={product.id} title={product.title} img = {product.image} price={product.price} ratingRate={product.rating.rate} ratingCount={product.rating.count} />
-        ))}
-      </div>
-    );
-  }
-  
-  export default ProductBox;
+
+  return (
+    <div className={styles.productBox}>
+      {data.map((product) => (
+        <Product
+          key={product.id}
+          title={product.title}
+          img={product.image}
+          price={product.price}
+          ratingRate={product.rating.rate}
+          ratingCount={product.rating.count}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ProductBox;
