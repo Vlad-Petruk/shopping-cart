025a92384da7dc3add1f4a37f7e13bff598b6619
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-16 11:56:56 +0300

    add empty cart page end conditional rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,47 +1,56 @@
 import { useOutletContext } from "react-router-dom";
-import CartProduct from "./CartPtoduct";
+import CartProduct from "./CartProduct";
 import styles from "./Cart.module.css"
 
 function Cart() {
-  const [cart, setCart, cartAmount, setCartAmount] = useOutletContext();
+  const [cart, setCart, cartAmount, setCartAmount] = useOutletContext(); 
 
-// There must be conditional rendering based on cart
-    return (
-      <div className={styles.cart}>
-        <div className={styles.productsBox}>
-          <div className={styles.h1}>YOUR CART</div>
-          <div className={styles.products}>
-          {cart.map((product) => (
-          <CartProduct
-            key={product.id}
-            title={product.title}
-            img={product.image}
-            price={product.price}
-            // deleteItem = {}
-          />
-      ))}
+ 
+  return (
+    <div className={styles.cart}>
+      {cart.length > 0 ? (
+        <>
+          <div className={styles.productsBox}>
+            <div className={styles.h1}>YOUR CART</div>
+            <div className={styles.products}>
+              {cart.map((product) => (
+                <CartProduct
+                  key={product.id}
+                  title={product.title}
+                  img={product.image}
+                  price={product.price}
+                  // deleteItem = {}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={styles.sumBox}>
-          <div className={styles.sum}>
-            <p className={styles.h2}>Order Summary</p>
-            <div className={styles.subtotal}>
-              <p>Subtotal</p>
-              <p className={styles.price}>price</p>
+          <div className={styles.sumBox}>
+            <div className={styles.sum}>
+              <p className={styles.h2}>Order Summary</p>
+              <div className={styles.subtotal}>
+                <p>Subtotal</p>
+                <p className={styles.price}>price</p>
+              </div>
+              <div className={styles.delivery}>
+                <p>Delivery fee</p>
+                <p className={styles.price}>$0</p>
+              </div>
+              <div className={styles.total}>
+                <p>Total</p>
+                <p className={styles.totalPrice}>$0</p>
+              </div>
+              <button className={styles.buyBtn}>Buy</button>
             </div>
-            <div className={styles.delivery}>
-              <p>Delivery fee</p>
-              <p className={styles.price}>$0</p>
-            </div>
-            <div className={styles.total}>
-              <p>Total</p>
-              <p className={styles.totalPrice}>$0</p>
-            </div>
-            <button className={styles.buyBtn}>Buy</button>
           </div>
-        </div>
-      </div>
-    );
-  }
+        </>
+      ) : (
+        <div className={styles.empty}>
+          <p>Your cart is empty</p>
+          <button className={styles.shopBtn}>Shop now</button>
+          </div>
+      )}
+    </div>
+  );
+}
   
   export default Cart;
