dbd1772c132756b0dd1279ed2177dcf780fd6869
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-16 13:44:33 +0300

    styled product in cart

--- src/components/Cart/CartProduct.jsx
+++ src/components/Cart/CartProduct.jsx
@@ -1,13 +1,25 @@
 import styles from "./Cart.module.css"
 
 function CartProduct ({title, img, price, deleteItem}) {
+    
 
     return (
         <div className={styles.product}>
             <img src={img} alt="title" />
-            <p>{title}</p>
-            <p>{price}</p>
-            <button onClick={deleteItem}>X</button>
+            <div className={styles.middleBox}>
+                <p className={styles.title}>{title}</p>
+                <p className={styles.itemPrice}>${price}</p>
+                
+            </div>
+            <div className={styles.btnSection}>
+                <button className={styles.delete} onClick={deleteItem}>X</button>
+                <div className={styles.plusMinusBox}>
+                        <div>−</div>
+                        <div className={styles.itemAmount}>0</div>
+                        <div>+</div>
+                </div>
+            </div>
+           
         </div>
     )
 }
