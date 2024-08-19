71f940273bc1d25c224afb22586e2eb83b3d0c25
Vlad-Petruk <vladpetruk00@gmail.com>
2024-08-19 18:21:55 +0300

    handle correct sum and item number when deleting

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,22 +1,38 @@
 import { useState, useEffect } from "react";
 import { useOutletContext, Link } from "react-router-dom";
 import CartProduct from "./CartProduct";
-import styles from "./Cart.module.css"
+import styles from "./Cart.module.css";
 
 function Cart() {
-  const [cart, setCart, cartAmount, setCartAmount] = useOutletContext(); 
+  const [cart, setCart, cartAmount, setCartAmount] = useOutletContext();
+  const [itemAmounts, setItemAmounts] = useState({});
   const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    const totalSum = cart.reduce((acc, product) => {
+      const amount = itemAmounts[product.id] || 1; 
+      return acc + product.price * amount;
+    }, 0);
+    setSum(totalSum.toFixed(2));
+  }, [cart, itemAmounts]);
+
+  const updateItemAmount = (id, newAmount) => {
+    setItemAmounts((prevAmounts) => ({
+      ...prevAmounts,
+      [id]: newAmount,
+    }));
+  };
 
   const deleteItem = (id) => {
     const updatedCart = cart.filter((product) => product.id !== id);
-    setCartAmount(cartAmount-1)
     setCart(updatedCart);
+    setCartAmount(cartAmount - 1);
+    setItemAmounts((prevAmounts) => {
+      const { [id]: _, ...rest } = prevAmounts;
+      return rest;
+    });
   };
 
-  const updateCart = (updatedCart) => {
-    setCart(updatedCart);
-};
- 
   return (
     <div className={styles.cart}>
       {cart.length > 0 ? (
@@ -27,13 +43,13 @@ function Cart() {
               {cart.map((product) => (
                 <CartProduct
                   key={product.id}
+                  id={product.id}
                   title={product.title}
                   img={product.image}
                   price={product.price}
+                  itemAmount={itemAmounts[product.id] || 1}
+                  updateItemAmount={updateItemAmount}
                   deleteItem={() => deleteItem(product.id)}
-                  setSum={setSum}
-                  cart={cart}
-                  updateCart={updateCart}
                 />
               ))}
             </div>
@@ -60,13 +76,13 @@ function Cart() {
       ) : (
         <div className={styles.empty}>
           <p>Your cart is empty</p>
-          <Link to={'/shop'}>
+          <Link to="/shop">
             <button className={styles.shopBtn}>Shop now</button>
           </Link>
-          </div>
+        </div>
       )}
     </div>
   );
 }
-  
-  export default Cart;
+
+export default Cart;
--- src/components/Cart/CartProduct.jsx
+++ src/components/Cart/CartProduct.jsx
@@ -1,67 +1,34 @@
-import { useState } from "react"
-import { useEffect } from "react";
-import styles from "./Cart.module.css"
+import styles from "./Cart.module.css";
 
-function CartProduct ({title, img, price, deleteItem, setSum, cart}) {
-    const [itemAmount, setItemAmount] = useState(1);
-    // useEffect(() => {
-    //     const newCart = [...cart];
-    //     const totalSum = newCart.reduce((acc, prod) => {
-    //         if (prod.id === title) {
-    //             return acc + prod.price * itemAmount;
-    //         }
-    //         return acc + prod.price;
-    //     }, 0);
-    //     setSum(totalSum.toFixed(2));
-    // }, [itemAmount, cart, setSum, title]);
+function CartProduct({ id, title, img, price, itemAmount, updateItemAmount, deleteItem }) {
+  
+  const increase = () => {
+    updateItemAmount(id, itemAmount + 1);
+  };
 
-    // function increase() {
-    //     setItemAmount((amount) => amount + 1);
-    // }
-
-    // function decrease() {
-    //     if (itemAmount > 1) {
-    //         setItemAmount((amount) => amount - 1);
-    //     }
-    // }
-    useEffect(()=>{
-        const newCart = [...cart]
-        const totalSum = newCart.reduce((acc, prod) => acc + prod.price, 0);
-        setSum(totalSum);
-      },[cart])
-
-    function increase() {
-        setItemAmount((amount) => amount + 1);
-        setSum((sum) => (parseFloat(sum) + price).toFixed(2));
+  const decrease = () => {
+    if (itemAmount > 1) {
+      updateItemAmount(id, itemAmount - 1);
     }
-    
-    function decrease() {
-        if (itemAmount > 1) {
-            setItemAmount((amount) =>amount - 1);
-            setSum((sum) => (parseFloat(sum) - price).toFixed(2));
-        }
-    }
-    
+  };
 
-    return (
-        <div className={styles.product}>
-            <img src={img} alt="title" />
-            <div className={styles.middleBox}>
-                <p className={styles.title}>{title}</p>
-                <p className={styles.itemPrice}>${price}</p>
-                
-            </div>
-            <div className={styles.btnSection}>
-                <button className={styles.delete} onClick={deleteItem}>X</button>
-                <div className={styles.plusMinusBox}>
-                        <div onClick={decrease}>−</div>
-                        <div className={styles.itemAmount}>{itemAmount}</div>
-                        <div onClick={increase}>+</div>
-                </div>
-            </div>
-           
+  return (
+    <div className={styles.product}>
+      <img src={img} alt={title} />
+      <div className={styles.middleBox}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.itemPrice}>${price}</p>
+      </div>
+      <div className={styles.btnSection}>
+        <button className={styles.delete} onClick={deleteItem}>X</button>
+        <div className={styles.plusMinusBox}>
+          <div onClick={decrease}>−</div>
+          <div className={styles.itemAmount}>{itemAmount}</div>
+          <div onClick={increase}>+</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default CartProduct
+export default CartProduct;
